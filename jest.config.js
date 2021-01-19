2f5094c9194a5eba78e60b120ee4beb61ca81d99
Michael Arnaldi <arnaldimichael@gmail.com>
2021-01-19 14:51:02 +0000

    fixes

--- jest.config.js
+++ jest.config.js
@@ -15,20 +15,8 @@ module.exports = {
   ],
   verbose: true,
   moduleNameMapper: {
-    "@effect-ts/jest/(.*)$": "<rootDir>/packages/jest/build/_traced/$1",
-    "@effect-ts/jest$": "<rootDir>/packages/jest/build/_traced",
     "@effect-ts/morphic/(.*)$": "<rootDir>/packages/morphic/build/_traced/$1",
-    "@effect-ts/morphic$": "<rootDir>/packages/morphic/build/_traced",
-    "@effect-ts/monocle/(.*)$": "<rootDir>/packages/monocle/build/_traced/$1",
-    "@effect-ts/monocle$": "<rootDir>/packages/monocle/build/_traced",
-    "@effect-ts/system/(.*)$": "<rootDir>/packages/system/build/_traced/$1",
-    "@effect-ts/system$": "<rootDir>/packages/system/build/_traced",
-    "@effect-ts/core/(.*)$": "<rootDir>/packages/core/build/_traced/$1",
-    "@effect-ts/core$": "<rootDir>/packages/core/build/_traced",
-    "@effect-ts/node/(.*)$": "<rootDir>/packages/node/build/_traced/$1",
-    "@effect-ts/node$": "<rootDir>/packages/node/build/_traced",
-    "@effect-ts/tracing-utils/(.*)$": "<rootDir>/packages/tracing-utils/build/$1",
-    "@effect-ts/tracing-utils$": "<rootDir>/packages/tracing-utils/build"
+    "@effect-ts/morphic$": "<rootDir>/packages/morphic/build/_traced"
   },
   globals: {
     "ts-jest": {
